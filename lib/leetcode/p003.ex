c52c119b90290e11851d7055054e0e8d9d9c5d5d
James Long <846867+jameslong@users.noreply.github.com>
2025-05-26 12:27:37 -0700

    Add solution for problem #3 (#3)
    * Add single pass solution
    
    * Add optimised solution (using bitmask)
    
    * Optimise via private functions + smaller (sometimes!) bitmasks
    
    * Add notes for P003

---
+++ lib/leetcode/p003.ex
@@ -0,0 +1,40 @@
+defmodule Leetcode.P003 do
+  @moduledoc """
+  Given a string s, find the length of the longest substring without duplicate characters.
+
+  Constraints:
+
+    0 <= s.length <= 5 * 104
+    s consists of English letters, digits, symbols and spaces.
+
+  """
+
+  @spec length_of_longest_substring(s :: String.t()) :: integer
+  def length_of_longest_substring(s) do
+    len = byte_size(s)
+    shrink_left(s, len - 1, len - 1, 0, 0)
+  end
+
+  defp shrink_left(_s, l, _r, _b, res) when l < 0, do: res
+
+  defp shrink_left(s, l, r, b, res) do
+    char = :binary.at(s, l) - ?\s
+    b_char = Bitwise.bsl(1, char)
+
+    if Bitwise.band(b, b_char) != 0 do
+      shrink_right(s, l, r, char, b, res)
+    else
+      shrink_left(s, l - 1, r, Bitwise.bor(b, b_char), max(res, r - l + 1))
+    end
+  end
+
+  defp shrink_right(s, l, r, dupe_char, b, res) do
+    char = :binary.at(s, r) - ?\s
+
+    if char == dupe_char do
+      shrink_left(s, l - 1, r - 1, b, res)
+    else
+      shrink_right(s, l, r - 1, dupe_char, Bitwise.bxor(b, Bitwise.bsl(1, char)), res)
+    end
+  end
+end
---
+++ test/leetcode/p003_test.exs
@@ -0,0 +1,16 @@
+defmodule Leetcode.P003Test do
+  use ExUnit.Case
+
+  alias Leetcode.P003
+
+  describe "length_of_longest_substring/1" do
+    test "with examples returns correct results" do
+      assert P003.length_of_longest_substring(" ") == 1
+      assert P003.length_of_longest_substring("abba") == 2
+      assert P003.length_of_longest_substring("uqinntq") == 4
+      assert P003.length_of_longest_substring("abcabcbb") == 3
+      assert P003.length_of_longest_substring("bbbbb") == 1
+      assert P003.length_of_longest_substring("pwwkew") == 3
+    end
+  end
+end
