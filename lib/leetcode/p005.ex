661cddcfd598fcf7e1af7b7be5ad84d23131ed67
jameslong <846867+jameslong@users.noreply.github.com>
2025-06-04 13:21:41 -0700

    Add optimised solution

--- lib/leetcode/p005.ex
+++ lib/leetcode/p005.ex
@@ -11,32 +11,38 @@ defmodule Leetcode.P005 do
 
   @spec longest_palindrome(s :: String.t()) :: String.t()
   def longest_palindrome(s) do
-    s
-    |> String.to_charlist()
-    |> longest_palindrome([])
-  end
-
-  defp longest_palindrome([], acc), do: List.to_string(acc)
-
-  defp longest_palindrome(list = [_h | t], acc) do
-    current = palindrome_starting_at(Enum.reverse(list))
-
-    if length(current) > length(acc) do
-      longest_palindrome(t, current)
-    else
-      longest_palindrome(t, acc)
-    end
-  end
-
-  defp palindrome_starting_at([]), do: []
-
-  defp palindrome_starting_at(s = [_ | t]) do
-    if palindrome?(s) do
+    {l, r} =
       s
+      |> String.to_charlist()
+      |> List.to_tuple()
+      |> do_longest_palindrome()
+
+    :binary.part(s, l, r - l + 1)
+  end
+
+  defp do_longest_palindrome(s) do
+    Enum.reduce(0..(tuple_size(s) - 1), {0, 0}, fn i, p_max ->
+      p_max
+      |> longest(expand_from_centre(s, i, i))
+      |> longest(expand_from_centre(s, i, i + 1))
+    end)
+  end
+
+  defp expand_from_centre(s, l, r), do: expand_from_centre(s, l, r, 0)
+
+  defp expand_from_centre(s, l, r, offset) when l - offset < 0 or r + offset >= tuple_size(s) do
+    {l - offset + 1, r + offset - 1}
+  end
+
+  defp expand_from_centre(s, l, r, offset) do
+    if elem(s, l - offset) == elem(s, r + offset) do
+      expand_from_centre(s, l, r, offset + 1)
     else
-      palindrome_starting_at(t)
+      {l - offset + 1, r + offset - 1}
     end
   end
 
-  defp palindrome?(chars), do: chars == Enum.reverse(chars)
+  defp longest({a_l, a_r} = a, {b_l, b_r} = b) do
+    if a_r - a_l >= b_r - b_l, do: a, else: b
+  end
 end
--- test/leetcode/p005_test.exs
+++ test/leetcode/p005_test.exs
@@ -5,6 +5,7 @@ defmodule Leetcode.P005Test do
 
   describe "longest_palindrome/1" do
     test "with examples returns correct results" do
+      assert P005.longest_palindrome("a") == "a"
       assert P005.longest_palindrome("babad") == "bab"
       assert P005.longest_palindrome("cbbd") == "bb"
     end
