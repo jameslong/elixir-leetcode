4a498af2c0de28f0a366cffeacc49b572c7d3ef3
James Long <846867+jameslong@users.noreply.github.com>
2025-06-27 10:11:29 -0700

    P015 (#16)
    * Add brute force solution
    
    * Add optimised solution
    
    * Add optimised solution
    
    * Add extra optimisations (early outs)
    
    * Add notes
    
    * Update README

---
+++ lib/leetcode/p015.ex
@@ -0,0 +1,73 @@
+defmodule Leetcode.P015 do
+  @moduledoc """
+  Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+  Notice that the solution set must not contain duplicate triplets.
+
+  Constraints:
+
+    3 <= nums.length <= 3000
+    -10^5 <= nums[i] <= 10^5
+
+  """
+
+  @spec three_sum(nums :: [integer]) :: [[integer]]
+  def three_sum(nums) do
+    nums = List.to_tuple(Enum.sort(nums))
+    do_three_sum(nums, 0, tuple_size(nums), [])
+  end
+
+  defp do_three_sum(nums, i, len, acc) do
+    val_i = elem(nums, i)
+
+    cond do
+      val_i > 0 or i > len - 3 ->
+        acc
+
+      i == 0 or elem(nums, i - 1) != val_i ->
+        acc = find_triplets(nums, i, i + 1, len - 1, acc)
+        do_three_sum(nums, i + 1, len, acc)
+
+      true ->
+        do_three_sum(nums, i + 1, len, acc)
+    end
+  end
+
+  defp find_triplets(_nums, _i, l, r, acc) when l >= r, do: acc
+
+  defp find_triplets(nums, i, l, r, acc) do
+    val_i = elem(nums, i)
+    val_l = elem(nums, l)
+    val_r = elem(nums, r)
+    sum = val_i + val_l + val_r
+
+    cond do
+      sum > 0 ->
+        find_triplets(nums, i, l, r - 1, acc)
+
+      sum < 0 ->
+        find_triplets(nums, i, l + 1, r, acc)
+
+      true ->
+        acc = [[val_i, val_l, val_r] | acc]
+        l = grow_from_left(nums, l + 1, r, val_l)
+        r = shrink_from_right(nums, l, r - 1, val_r)
+
+        find_triplets(nums, i, l, r, acc)
+    end
+  end
+
+  defp shrink_from_right(nums, l, r, prev_val_r) do
+    cond do
+      r <= l or elem(nums, r) != prev_val_r -> r
+      true -> shrink_from_right(nums, l, r - 1, prev_val_r)
+    end
+  end
+
+  defp grow_from_left(nums, l, r, prev_val_l) do
+    cond do
+      l >= r or elem(nums, l) != prev_val_l -> l
+      true -> grow_from_left(nums, l + 1, r, prev_val_l)
+    end
+  end
+end
---
+++ test/leetcode/p015_test.exs
@@ -0,0 +1,13 @@
+defmodule Leetcode.P015Test do
+  use ExUnit.Case
+
+  alias Leetcode.P015
+
+  describe "three_sum/1" do
+    test "with examples returns correct results" do
+      assert P015.three_sum([-1, 0, 1, 2, -1, -4]) == [[-1, 0, 1], [-1, -1, 2]]
+      assert P015.three_sum([0, 1, 1]) == []
+      assert P015.three_sum([0, 0, 0]) == [[0, 0, 0]]
+    end
+  end
+end
